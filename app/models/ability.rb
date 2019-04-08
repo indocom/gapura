05d4deb1f657beb54851025fa6e6b72b4df76a6f
Steven Wijaya <stevenwjy149@gmail.com>
2019-04-09 00:09:33 +0800

    Fix Past nuansa overview (#15)
    * Change background image for Past NUANSA overview
    
    * Add semi-transparent textbox
    
    * Fix issue: mobile version of past NUANSA overview
    
    * Fix some code styling issues
    
    * Add content to past NUANSA overview and fix some styling issues

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.present?
-      if user.has_role? :admin
-        if user.has_role? :superuser
-          can [:create, :update, :destroy], User
-        end
+    return unless user.present?
+    if user.has_role? :admin
+      if user.has_role? :superuser
+        can [:create, :update, :destroy], User
       end
     end
   end

400768492892e38ccb492de5703f7409a3eccaf6
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-04-09 22:05:24 +0800

    Add feature to create Past Event

--- app/controllers/past_events_controller.rb
+++ app/controllers/past_events_controller.rb
@@ -14,16 +14,19 @@ class PastEventsController < ApplicationController
 
   # GET /past_events/new
   def new
+    ensure_admin || return
     @past_event = PastEvent.new
   end
 
   # GET /past_events/1/edit
   def edit
+    ensure_admin || return
   end
 
   # POST /past_events
   # POST /past_events.json
   def create
+    ensure_admin || return
     @past_event = PastEvent.new(past_event_params)
 
     respond_to do |format|
@@ -40,6 +43,7 @@ class PastEventsController < ApplicationController
   # PATCH/PUT /past_events/1
   # PATCH/PUT /past_events/1.json
   def update
+    ensure_admin || return
     respond_to do |format|
       if @past_event.update(past_event_params)
         format.html { redirect_to @past_event, notice: 'Past event was successfully updated.' }
@@ -54,6 +58,7 @@ class PastEventsController < ApplicationController
   # DELETE /past_events/1
   # DELETE /past_events/1.json
   def destroy
+    ensure_admin || return
     @past_event.destroy
     respond_to do |format|
       format.html { redirect_to past_events_url, notice: 'Past event was successfully destroyed.' }
@@ -69,6 +74,6 @@ class PastEventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def past_event_params
-      params.fetch(:past_event, {})
+      params.require(:past_event).permit(:title, :subtitle, :description, :year)
     end
 end

fcbd83105ac3944d04c8b7106a3f7e99181797c2
Christian James Welly <chrisjwelly@gmail.com>
2019-05-22 14:00:31 +0800

    Add navigation tooltips (appears on hover)

--- vendor/assets/javascripts/fullpage/past_nuansa.js
+++ vendor/assets/javascripts/fullpage/past_nuansa.js
@@ -3,8 +3,14 @@ new fullpage('#fullpage', {
   autoScrolling:true,
   scrollHorizontally: true,
   scrollOverflow: true,
+  anchors: ["nuansa2018", "nuansa2017", "nuansa2015", "nuansa2014", 
+            "nuansa2013", "nuansa2012", "nuansa2011", "nuansa2010", 
+            "nuansa2009", "nuansa2008" ],
   navigation: true,
   navigationPosition: 'right',
+  navigationTooltips: ["2018", "2017", "2015", "2014", 
+                       "2013", "2012", "2011", "2010", 
+                       "2009", "2008" ],
 });
 
 //methods

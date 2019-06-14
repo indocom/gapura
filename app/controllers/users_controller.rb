505fc3db0095a0a40d1f90a4a49dd7c443e49989
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-06-15 04:43:19 +0800

    Render 404 if user did not exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,11 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      begin
+        @user = User.find(params[:id])
+      rescue
+        not_found
+      end
     end
 
     # Only allow a trusted parameter "white list" through.

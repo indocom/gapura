68ec3f62d15c8b8a47bb48f8cac8e444baa36f22
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-06-19 21:46:51 +0800

    Add a placeholder email configuration in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {:host => 'nuansacp.com'}
+  config.action_mailer.delivery_method = :smtp
+  # Assuming smtp server exist in port 25
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'nuansacp.com'
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false

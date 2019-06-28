ba9e177bb9fc0af41b01527b98a8240b907d1061
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-06-28 23:16:24 +0800

    Remove layout at 404 Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def render_404
-    render file: "#{Rails.root}/public/404", status: :not_found
+    render file: "#{Rails.root}/public/404", layout: false, status: :not_found
   end
   
   def configure_permitted_parameters

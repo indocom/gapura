186414e018a55eaa2464e0de5097fb1c2ddfea9e
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-07-06 16:50:46 +0800

    Configure sponsors association with event

---
+++ app/models/sponsor.rb
@@ -0,0 +1,15 @@
+class Sponsor < ApplicationRecord
+  has_one_attached :logo
+  belongs_to  :event, foreign_key: :year, primary_key: :year
+
+  validate :logo_validation
+
+  def logo_validation
+    if logo.attached?
+      if logo.blob.byte_size > 2097152
+        logo.purge
+        errors[:base] << 'Sponsor logo too big'
+      end
+    end
+  end
+end

409adb5d30d819d5dbc75a26a757db7abec5a958
stevenwjy <stevenwjy149@gmail.com>
2019-07-06 20:12:55 +0800

    Fix schema after db:setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_07_06_082919) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,6 +112,6 @@ ActiveRecord::Schema.define(version: 2019_07_06_082919) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "event_info", "events", column: "year", primary_key: "year"
-  add_foreign_key "sponsors", "events", column: "year", primary_key: "year"
   add_foreign_key "gallery_photos", "events", column: "year", primary_key: "year"
+  add_foreign_key "sponsors", "events", column: "year", primary_key: "year"
 end

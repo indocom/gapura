65214f654ee20ef83e7d75582faf70811c7dbef1
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-07-06 12:42:40 +0800

    Configure association between events and gallery_photos

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ApplicationRecord
   has_one :event_info, dependent: :destroy, foreign_key: :year,
     primary_key: :year, inverse_of: :event
   accepts_nested_attributes_for :event_info
+  has_many :gallery_photos, dependent: :destroy, foreign_key: :year,
+    primary_key: :year, inverse_of: :event
 end
--- app/models/gallery_photo.rb
+++ app/models/gallery_photo.rb
@@ -1,3 +1,12 @@
 class GalleryPhoto < ApplicationRecord
   has_one_attached :image
+  belongs_to :event, foreign_key: :year, primary_key: :year, touch: true
+
+  def create(attributes = nil, &block)
+    gallery_photo = super
+    if gallery_photo.image.attached?
+      gallery_photo.image_link = url_for(gallery_photo)
+    end
+    return gallery_photo
+  end
 end

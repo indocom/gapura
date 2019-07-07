438d424d90e9c671ee141a7c6a3364bd600f3e21
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-07-07 13:16:24 +0800

    Added seed value for testimony and use env variable for superuser

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-superuser = User.create(username: 'superuser', email: 'superuser@example.com',
-                        password: '123456', confirmed_at: DateTime.now)
+superuser = User.create(username: ENV['DEFAULT_USERNAME'], email: ENV['DEFAULT_EMAIL'],
+                        password: ENV['DEFAULT_PASSWORD'], confirmed_at: DateTime.now)
 superuser.add_role :admin
 superuser.add_role :superuser
 
-admin = User.create(username: 'admin', email: 'admin@example.com',
-                    password: '123456', confirmed_at: DateTime.now)
-admin.add_role :admin
+if (Rails.env == "development")
+  admin = User.create(username: 'admin', email: 'admin@example.com',
+                      password: '123456', confirmed_at: DateTime.now)
+  admin.add_role :admin
 
-user = User.create(username: 'user', email: 'user@example.com',
-                    password: '123456', confirmed_at: DateTime.now)
+  user = User.create(username: 'user', email: 'user@example.com',
+                     password: '123456', confirmed_at: DateTime.now)
+
+  10.times do |i|
+    testimonial = Testimony.create(name: "Pokemon #{i}", profession: "Audience #{i}",
+                                   testimony: "Good #{i}. " * 20)
+    testimonial.photo.attach(
+      io: File.open( Rails.root.join('app', 'assets', 'images', 'testimonials', "testimonial-#{i % 3}.png")),
+      filename: "testimonial-#{i % 3}.png", content_type: 'image/png'
+    )
+
+    testimonial.save
+  end
+end

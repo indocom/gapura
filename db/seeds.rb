66ea9f2de3502c4ecf54d023392f0b8eb4f3d52f
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-07-07 14:57:53 +0800

    Added FAQ seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,4 +29,8 @@ if (Rails.env == "development")
 
     testimonial.save
   end
+
+  10.times do |i|
+    FrequentlyAskedQuestion.create(question: "Question #{i} " * 5 + "?", answer: "Answer #{i} " * 20 + "." )
+  end
 end

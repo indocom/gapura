4e15f59bee117fd2dd4ccc541f869d5ee304a9f3
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-07-13 20:26:53 +0800

    Created image_form partial and configure image mapping from params

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,7 @@
 class ImagesController < ApplicationController
   def serve
     @image = Image.find(params[:id])
-    send_data(@image.data, type: @image.mime_type, filename: @image.file_name, disposition: 'inline')
+    send_data(@image.data, type: @image.mime_type, filename: @image.filename, disposition: 'inline')
   rescue
     not_found
   end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,14 @@
 class Image < ApplicationRecord
   belongs_to :imageable, polymorphic: true, touch: true
+  attr_accessor :file
+
+  before_validation :populate_fields_from_file
+
+  private
+    def populate_fields_from_file
+      return if file.nil?
+      self.data = file.read
+      self.filename = file.original_filename
+      self.mime_type = file.content_type
+    end
 end

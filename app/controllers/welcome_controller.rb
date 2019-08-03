881c9fb80e0a3c46fbdd940dbe7ad7666f68f408
Mario Lorenzo <mario@nubela.co>
2019-08-03 17:59:59 +0800

    modify load more for gallery_photos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,24 @@ class WelcomeController < ApplicationController
   include UrlHelper
 
   def index
+    @current_event = Event.find_by(year: current_year)
     @testimonies = Testimony.all
     @faqs = FrequentlyAskedQuestion.all
     @partners = Event.find_by(year: current_year).sponsors
-  rescue
-    not_found
+
+    if params[:total]
+      @gallery_photos = @current_event.gallery_photos.limit(8).offset(params[:total])
+      @current_counted = params[:total].to_i() + @gallery_photos.count
+    else
+      @gallery_photos = @current_event.gallery_photos.limit(8)
+      @current_counted = @gallery_photos.size
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+    rescue
+      not found 
   end
 end

9c9e1b53acdc5411f4f8d23edb6f4064cc4ccee2
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-09-11 00:21:55 +0800

    Adds action mailer for sending confirmation email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
   layout 'mailer'
+
+  def ticket_confirmation
+    @customer = params[:customer]
+    @claim_url = claim_ticket_url(claim_token: @customer.claim_token)
+    mail(to: @customer.email, subject: 'NUANSA Ticket QR Code')
+  end
 end

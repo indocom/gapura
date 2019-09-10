13d7514c37885b9371be793fcaf556b377ae23da
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-09-11 02:21:31 +0800

    Adds inline attachment QR Code

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,7 +3,10 @@ class ApplicationMailer < ActionMailer::Base
 
   def ticket_confirmation
     @customer = params[:customer]
-    @claim_url = claim_ticket_url(claim_token: @customer.claim_token)
+
+    claim_url = claim_ticket_url(claim_token: @customer.claim_token)
+    attachments.inline['QRCode.png'] = RQRCode::QRCode.new(claim_url).as_png.to_s
+
     mail(to: @customer.email, subject: 'NUANSA Ticket QR Code')
   end
 end

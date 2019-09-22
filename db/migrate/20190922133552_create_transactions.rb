0eb101a505b352280169aef605d26e8193935e1a
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-09-22 23:00:14 +0800

    Adds email column for transaction entity

--- db/migrate/20190922133552_create_transactions.rb
+++ db/migrate/20190922133552_create_transactions.rb
@@ -3,8 +3,9 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
     create_table :transactions do |t|
       t.datetime :created_at, null: false
       t.datetime :purchased_at, null: false
-      t.string :ticket_type, null: false, default: ""
       t.string :booking_reference, null: false, default: ""
+      t.string :email, null: false, default: ""
+      t.string :ticket_type, null: false, default: ""
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -130,8 +130,9 @@ ActiveRecord::Schema.define(version: 2019_09_22_134544) do
   create_table "transactions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "purchased_at", null: false
-    t.string "ticket_type", default: "", null: false
     t.string "booking_reference", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "ticket_type", default: "", null: false
   end
 
   create_table "users", force: :cascade do |t|

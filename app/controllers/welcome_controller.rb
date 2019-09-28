a2ce7d1276ad2f4e7b476db1c9f082b7e938533d
Steven Wijaya <stevenwjy149@gmail.com>
2019-09-28 15:50:13 +0800

    Order sponsors by type and update minor ui for faq (#126)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
     @current_event = Event.find_by(year: current_year)
     @testimonies = Testimony.all
     @faqs = FrequentlyAskedQuestion.all
-    @partners = @current_event.sponsors
+    @partners = @current_event.sponsors.order(:type)
     @gallery_photos = @current_event.gallery_photos.limit(8)
     @more_photos = @current_event.gallery_photos.count > 8
   rescue

e7d1d9f97c46662e5796866facdd229cfbf342d7
Steven Wijaya <stevenwjy149@gmail.com>
2019-10-13 21:17:30 +0800

    Update email subject for e-ticket

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -7,6 +7,6 @@ class ApplicationMailer < ActionMailer::Base
 
     attachments.inline['QRCode.png'] = RQRCode::QRCode.new(claim_url).as_png.to_s
 
-    mail(to: @ticket.customer.email, subject: 'NUANSA Ticket QR Code')
+    mail(to: @ticket.customer.email, subject: 'NUANSA 2019: Lukisan Jati E-Ticket')
   end
 end

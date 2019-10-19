606d2991022b79e3878ff599b0656779d286d1c2
fchensan <chensanfrancisco@gmail.com>
2019-10-19 21:12:17 +0800

    Set receive_marketing_email in Subscriber default to true

--- db/migrate/20190926130108_create_subscribers.rb
+++ db/migrate/20190926130108_create_subscribers.rb
@@ -2,7 +2,7 @@ class CreateSubscribers < ActiveRecord::Migration[5.2]
   def change
     create_table :subscribers do |t|
       t.references :customer, foreign_key: true
-      t.boolean :receive_marketing_email
+      t.boolean :receive_marketing_email, :default => true
       t.string :unsubscribe_code
 
       t.timestamps

4733b0ec73d63b8505b449f89e6a1262cdb7e2e4
Steven Wijaya <stevenwjy149@gmail.com>
2019-10-20 01:06:58 +0800

    Deployment 0.0.4 (#148)
    * Add claim page (#146)
    
    * Add claim history seed
    
    * Fix view for claim page
    
    * Add redeem action
    
    * Update notice message
    
    * Add navigation from index to claim page for superuser
    
    * Add validation on claim history
    
    * Update app/assets/stylesheets/admin/shared.scss
    
    * Update view for claim page
    
    * Enable admin to create mock ticket (#147)
    
    * Enable admin to create mock ticket
    
    * update error message
    
    * Add support to destroy mock tickets at once
    
    * Fix bug clear mock tickets
    
    * Restrict access to send confirmation email

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class TicketsController < ApplicationController
     before_action :ensure_admin
-    before_action :ensure_superuser, only: :destroy
+    before_action :ensure_superuser, only: [:destroy, :new, :create, :send_confirmation_email]
 
     def index
       @tickets = Ticket.includes(:customer).order(:purchased_at)
@@ -13,36 +13,51 @@ module Admin
       not_found
     end
 
-    # This method is currently outdated.
+    def new
+      @ticket = Ticket.new
+    end 
+
     def create
-      fail
-      customer = Customer.find_or_create_by(email: current_user.email)
-      ticket = customer.tickets.create(ticket_type: 'Mock', purchased_at: DateTime.now)
-      ticket.send_confirmation_email
-      flash[:notice] = "Ticket has been created!!!"
-      redirect_to admin_ticket_url(ticket)
+      new_ticket = create_ticket_from_mock_data(mock_ticket_params)
+      new_ticket.send_confirmation_email
+
+      flash[:notice] = 'Ticket has been created!!!'
+      redirect_to admin_ticket_url(new_ticket)
     rescue
       not_found
     end
 
-    # This method is currently outdated.
     def claim
-      fail
-      @customer = Customer.find_by!(claim_token: params['claim_token'])
-      @tickets = @customer.tickets.order(:purchased_at)
+      @ticket = Ticket.find_by!(claim_token: params['claim_token'])
     rescue
       not_found
     end
 
-    # This method is currently outdated.
     def redeem
-      fail
+      @ticket = Ticket.find_by!(claim_token: params[:claim_token])
+      total_claim = params[:quantity]
+
+      @ticket.claim_histories.create!(claimed_by: current_user.username, claimed_at: DateTime.now,
+          claim_quantity: total_claim)
+
+      redirect_to admin_claim_ticket_url(claim_token: @ticket.claim_token), notice: "You are claiming for"\
+          " #{total_claim} #{'people'.pluralize(total_claim)}"
+    rescue
+      redirect_to admin_claim_ticket_url(claim_token: @ticket.claim_token), flash: { popup_alert:
+          "There is an error in claiming the ticket." }
     end
 
     def destroy
       @ticket = Ticket.find(params[:id])
       @ticket.destroy
       redirect_to admin_tickets_url, notice: 'Ticket was successfully destroyed.'
+    end
+
+    def clear_mock_tickets
+      Ticket.where(ticket_type: "MOCK").destroy_all
+      redirect_to admin_tickets_url, notice: 'All mock tickets have been successfully destroyed.'
+    rescue
+      redirect_to admin_tickets_url, flash: { popup_alert: 'Failed to destroy all mock tickets.' }
     end
 
     def send_confirmation_email
@@ -52,5 +67,31 @@ module Admin
     rescue
       redirect_to admin_tickets_url, flash: { popup_alert: 'Confirmation email failed to be sent.' }
     end
+
+    private 
+      def mock_ticket_params
+        params.require(:ticket).permit(:name, :email, :quantity)
+      end
+
+      def create_ticket_from_mock_data(ticket_info)
+        customer = Customer.find_or_create_by!(email: ticket_info[:email].downcase)
+        ticket = nil
+        5.times do |index|
+          begin
+            ticket = customer.tickets.create!(
+              booking_reference: SecureRandom.base58(5),
+              ticket_type: "MOCK",
+              name: ticket_info[:name],
+              quantity: ticket_info[:quantity],
+              purchased_at: DateTime.now
+            )
+            break
+          rescue
+            raise if (index == 4)
+          end 
+        end
+
+        return ticket
+      end
   end
 end
--- app/models/claim_history.rb
+++ app/models/claim_history.rb
@@ -1,3 +1,12 @@
 class ClaimHistory < ApplicationRecord
   belongs_to :ticket, inverse_of: :claim_histories
+
+  validate :total_claim_cannot_exceed_quantity, on: :create
+
+  private
+    def total_claim_cannot_exceed_quantity
+      if (ticket.total_unclaimed < claim_quantity)
+        errors.add(:claim_quantity, "can't exceed remaining quantity")
+      end
+    end
 end

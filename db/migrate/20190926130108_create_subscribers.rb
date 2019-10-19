f67623370846858cb486c8b3138229f40a4c4e18
fchensan <chensanfrancisco@gmail.com>
2019-10-19 20:49:08 +0800

    Correct column order

--- db/migrate/20190926130108_create_subscribers.rb
+++ db/migrate/20190926130108_create_subscribers.rb
@@ -1,8 +1,8 @@
 class CreateSubscribers < ActiveRecord::Migration[5.2] 
   def change
     create_table :subscribers do |t|
-      t.boolean :receive_marketing_email
       t.references :customer, foreign_key: true
+      t.boolean :receive_marketing_email
       t.string :unsubscribe_code
 
       t.timestamps

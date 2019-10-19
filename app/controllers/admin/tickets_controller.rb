68883489943020a13d452d7f81e1497cf652c74e
Steven Wijaya <stevenwjy149@gmail.com>
2019-10-20 01:03:50 +0800

    Enable admin to create mock ticket (#147)
    * Enable admin to create mock ticket
    
    * update error message
    
    * Add support to destroy mock tickets at once
    
    * Fix bug clear mock tickets
    
    * Restrict access to send confirmation email

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class TicketsController < ApplicationController
     before_action :ensure_admin
-    before_action :ensure_superuser, only: :destroy
+    before_action :ensure_superuser, only: [:destroy, :new, :create, :send_confirmation_email]
 
     def index
       @tickets = Ticket.includes(:customer).order(:purchased_at)
@@ -13,14 +13,16 @@ module Admin
       not_found
     end
 
-    # This method is currently outdated.
+    def new
+      @ticket = Ticket.new
+    end 
+
     def create
-      fail
-      customer = Customer.find_or_create_by(email: current_user.email)
-      ticket = customer.tickets.create(ticket_type: 'Mock', purchased_at: DateTime.now)
-      ticket.send_confirmation_email
-      flash[:notice] = "Ticket has been created!!!"
-      redirect_to admin_ticket_url(ticket)
+      new_ticket = create_ticket_from_mock_data(mock_ticket_params)
+      new_ticket.send_confirmation_email
+
+      flash[:notice] = 'Ticket has been created!!!'
+      redirect_to admin_ticket_url(new_ticket)
     rescue
       not_found
     end
@@ -51,6 +53,13 @@ module Admin
       redirect_to admin_tickets_url, notice: 'Ticket was successfully destroyed.'
     end
 
+    def clear_mock_tickets
+      Ticket.where(ticket_type: "MOCK").destroy_all
+      redirect_to admin_tickets_url, notice: 'All mock tickets have been successfully destroyed.'
+    rescue
+      redirect_to admin_tickets_url, flash: { popup_alert: 'Failed to destroy all mock tickets.' }
+    end
+
     def send_confirmation_email
       @ticket = Ticket.find(params[:id])
       @ticket.send_confirmation_email
@@ -58,5 +67,31 @@ module Admin
     rescue
       redirect_to admin_tickets_url, flash: { popup_alert: 'Confirmation email failed to be sent.' }
     end
+
+    private 
+      def mock_ticket_params
+        params.require(:ticket).permit(:name, :email, :quantity)
+      end
+
+      def create_ticket_from_mock_data(ticket_info)
+        customer = Customer.find_or_create_by!(email: ticket_info[:email].downcase)
+        ticket = nil
+        5.times do |index|
+          begin
+            ticket = customer.tickets.create!(
+              booking_reference: SecureRandom.base58(5),
+              ticket_type: "MOCK",
+              name: ticket_info[:name],
+              quantity: ticket_info[:quantity],
+              purchased_at: DateTime.now
+            )
+            break
+          rescue
+            raise if (index == 4)
+          end 
+        end
+
+        return ticket
+      end
   end
 end

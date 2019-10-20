4466996406a0c5fddd5bf759c7d5767398f2857f
fchensan <chensanfrancisco@gmail.com>
2019-10-20 14:14:15 +0800

    Add email blaster preview

--- spec/mailers/previews/application_mailer_preview.rb
+++ spec/mailers/previews/application_mailer_preview.rb
@@ -2,4 +2,9 @@ class ApplicationMailerPreview < ActionMailer::Preview
   def ticket_confirmation
     ApplicationMailer.with(ticket: Ticket.second).ticket_confirmation
   end
+
+  def mail_personal
+  	ApplicationMailer.with(subscriber: Subscriber.second, 
+  		subject: "Test Subject", body: "<b>Hi!</b>").mail_personal
+  end
 end
